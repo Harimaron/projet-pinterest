5efc18055375f5531718d5d8dd6dfe37e03d1ee3
Valerian Thomas <thomasvalerian93@gmail.com>
2017-11-21 12:09:44 +0100

    adding MV

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$action=$_GET['action'];
+
+
+swith($action)
+{
+  case 'login':
+  include "Views/login.php";
+  break;
+  case 'admin':
+    include "Views/admin.php";
+  break;
+  case 'home':
+    include "Views/home.php";
+  break;
+  case 'signup':
+    include "Views/signup.php";
+  break;
+  case 'upload':
+    include "Views/upload.php";
+  break;
+}
+
+?>
