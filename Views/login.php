dedd49f602a32bae543d521ce562cf84b48a1716
Laureenb <laureen_bataille@hotmail.com>
2017-11-22 11:58:28 +0100

    formulaires + footer (html/css)

---
+++ Views/login.php
@@ -0,0 +1,11 @@
+<form action="index.php?action=login" method="post">
+
+	<label for="pseudo">Entrez votre pseudo</label>
+	<input type="text" name="pseudo" id="login_form"/><br/><br/>
+
+	<label for="password">Entrez votre mot de passe</label>
+	<input type="text" name="password" id="login_form"/><br/><br/>
+
+	<input type="submit" value="Log in" id="login_button"/><br/><br/>
+
+</form>
---
+++ Views/upload.php
@@ -0,0 +1,14 @@
+<form action="index.php" method="post">
+
+	<label for="url">Importez votre image</label>
+	<input type="text" name="url" id="up_image"/><br/><br/>
+
+	<label for="titre">Entrez un titre</label>
+	<input type="text" name="titre" id="up_title"/><br/><br/>
+
+	<label for="description">Description</label>
+	<input type="text" name="description" id="up_description"/><br/><br/>
+
+ 	<input type="submit" value="Valider" id="up_button"/><br/><br/>
+
+</form>
