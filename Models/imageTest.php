7aca39a0b9196ab5010b1041f817b39268489fc3
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2017-11-22 14:52:16 +0100

    photo move test

--- Models/imageTest.php
+++ Models/imageTest.php
@@ -4,7 +4,7 @@ $image=$_FILES["url"];
 $tmp=$image["tmp_name"];
 $name=$image["name"];
 $type=$image["type"];
-$filepath="imageBank/".$name;
+$filepath="../imageBank/".$name;
 
 move_uploaded_file($tmp,$filepath);
 
