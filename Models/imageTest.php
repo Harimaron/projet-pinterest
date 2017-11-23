060b06d8c95a2b1d6968f2ee933ba063ded6b5e0
Valerian Thomas <thomasvalerian93@gmail.com>
2017-11-23 14:49:38 +0100

    Syl G-Woman

--- Models/imageTest.php
+++ Models/imageTest.php
@@ -1,11 +1,32 @@
 <?php
 
-$image=$_FILES["url"];
-$tmp=$image["tmp_name"];
-$name=$image["name"];
-$type=$image["type"];
-$filepath="../imageBank/".$name;
 
-move_uploaded_file($tmp,$filepath);
+
+require '../vendor/autoload.php';
+
+
+use Intervention\Image\ImageManagerStatic as Image;
+
+Image::configure(array('driver' => 'imagick'));
+
+$img = Image::make($_FILES["url"]["tmp_name"]);
+
+$img->save('../big-imageBank/'.$_FILES["url"]["name"]);
+
+$img->resize(320, 240);
+
+
+$img->save('../mini-imageBank/'.$_FILES["url"]["name"]);
+
+// $image=$_FILES["url"];
+// $tmp=$image["tmp_name"];
+// $name=$image["name"];
+// $type=$image["type"];
+// $filepath="../big-imageBank/".$name;
+//
+//
+// move_uploaded_file($tmp,$filepath);
+
+
 
 ?>
