b5d1e0af815ca9a2762873820a1fc89488f91d0d
Harimaron <gaetan.ribonnet@outlook.com>
2017-11-24 14:23:52 +0100

    +header

--- Views/header.php
+++ Views/header.php
@@ -1,7 +1,19 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
+<html >
+<head>
+  <title>Header</title>
+      <link rel="stylesheet" href="Assets/header.css">
+      <meta charset="ISO-8859-1"> 
+</head>
+<body>
+    <div class = "header-wrapper">
+      <div class = "logo">
+        <img src="https://github.com/Harimaron/projet-pinterest/blob/master/Views/Assets/images/logo.png?raw=true" alt="logo">
+      </div>
+      <div class = "search" >
+      <input type="text" name="search-header" placeholder="Search"> </div>
+      <div class ="settings">        <img src="https://i.imgur.com/hND4Yuh.png" alt="logo">
+      </div>
+</div>    
+</body>
+</html>
