afad3aaea690a950bb391bac1419c1f5c8124e09
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2017-11-27 09:31:46 +0100

    suppresion de la parenthèse dans mysql qui empechais la recherche de texte dans mysql

--- Models/photo.php
+++ Models/photo.php
@@ -24,7 +24,7 @@ class photo
 
   public function getAllImage($search='')
   {
-    $req=$this->db->prepare("SELECT * FROM images WHERE description LIKE'%(?)%'");
+    $req=$this->db->prepare("SELECT * FROM images WHERE description LIKE'%?%'");
     $req->execute([$search]);
     $resultat=$req->fetchAll();
     return $resultat;
