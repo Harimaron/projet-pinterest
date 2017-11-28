9f972f7cadc5b73ff87d94968be3744421ceb60c
Harimaron <gaetan.ribonnet@outlook.com>
2017-11-28 11:46:24 +0100

    links

--- Views/login.php
+++ Views/login.php
@@ -12,20 +12,20 @@
 <body>
 	<!-- HEADER STARTS HERE -->
 	<div class="header-wrapper">
-		<a href="http://google.com">
-		<div class="logo-header"><img alt="logo" src="https://i.imgur.com/awbs7PX.png"></div></a> <a href="http://google.com">
+		<a href="index.php?action=home">
+		<div class="logo-header"><img alt="logo" src="https://i.imgur.com/awbs7PX.png"></div></a> <a href="index.php?action=uploadPage">
 		<div class="upload-header"><img alt="logo" src="https://i.imgur.com/yKgtMFi.png"></div></a>
 		<div class="search-header">
 			<input name="search-header-bis" placeholder="" type="text">
 		</div><a href="http://google.com">
-		<div class="admin-header"><img alt="logo" src="https://i.imgur.com/PkGEZTB.png"></div></a> <a href="http://google.com">
+		<div class="admin-header"><img alt="logo" src="https://i.imgur.com/PkGEZTB.png"></div></a> <a href="index.php?action=logout">
 		<div class="logout-header"><img alt="logo" src="https://i.imgur.com/UJ1Y6RI.png"></div></a>
 	</div><!-- HEADER ENDS HERE -->
 	<div class="login-page">
 		<div class="form">
 			<form action="index.php?action=loginData" class="login-form">
 				<input placeholder="username" type="text"> <input placeholder="password" type="password"> <button>login</button>
-				<p class="message">Not registered? <a href="#">Create an account</a></p>
+				<p class="message">Not registered? <a href="index.php?action=signup">Create an account</a></p>
 			</form>
 		</div>
 	</div>
